c8bc937f629724c39a5f58df8cf8022bc6cc35fe
Bethany Johnson <bethanywillfixit@gmail.com>
2022-03-09 14:17:17 -0600

    Update python_jobs_scheduler.py
    Working Threaded jobs scheduler.

--- backend-southern-exposure/python_jobs_scheduler.py
+++ backend-southern-exposure/python_jobs_scheduler.py
@@ -4,28 +4,46 @@ import time
 import schedule
 from threading import Thread
 
+import RPi.GPIO as GPIO
+import time
 
+pinList = [13,15]
+
+GPIO.setmode(GPIO.BOARD)
+
+for i in pinList:
+    GPIO.setup(i, GPIO.OUT)
+    GPIO.output(i, GPIO.LOW)
 
 accepted_threads = []
 threads = []
 
-count = 0
-
-def job(name):
+def job(name,pin):
+  if name.startswith("Test_Tomato_Sensor"):
+    seconds = 11
+  else:
+    seconds = 4
   print("I worked for" + name)
+  GPIO.output(pin, GPIO.HIGH)
+  time.sleep(seconds)
+  GPIO.output(pin, GPIO.LOW)
   print(threading.currentThread().ident)
   #pass
   
 def job_task(name):
   print(name)
-  
+
 
 def threaded_function_start(name_var):
-  thisJob = schedule.every(5).seconds.do(job, name = name_var)
+  if name_var.startswith("Test_Tomato_Sensor"):
+    gpio_pin = 13
+  else:
+    gpio_pin = 15
+  thisJob = schedule.every().minute.at(":23").do(job, name = name_var, pin = gpio_pin )
+  #thisJob = schedule.every(5).seconds.do(job, name = name_var, pin = gpio_pin )
   print("reached threaded "+name_var)
   while True:
     global accepted_threads
-    
     if name_var not in accepted_threads:
       schedule.cancel_job(thisJob)
       print("oh God I gotta kill myself")
@@ -37,9 +55,7 @@ def threaded_function_start(name_var):
 
 
 def connect_db():
-  #global accepted_threads
-  #global threads
-  threading.Timer(8.0, connect_db).start()
+  threading.Timer(1.0, connect_db).start()
   try:
     sqliteConnection = sqlite3.connect('./southern_exposure_database.db')
     cursor = sqliteConnection.cursor()
@@ -65,10 +81,12 @@ def connect_db():
         accepted_threads.remove(each)
       print("Jobs to be Added:")
       print(list(set(jobs_on_table) - set(accepted_threads)))
-      threads = []
+      #threads = []
       for each  in list(set(jobs_on_table) - set(accepted_threads)):
         accepted_threads.append(each)
-        threads.append(Thread(target = threaded_function_start(each)))
+        th = Thread(target = threaded_function_start(each))
+        th.start()
+        threads.append(th)
       for thread in threads:
         thread.start()
         #thread.join()
