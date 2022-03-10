5b139d65b1366762ca3b31b627843447840675cb
Chris K <ckaufmann@connamara.com>
2022-03-10 16:50:07 -0600

    Bump out get_processes into a sub, move variables

--- backend-southern-exposure/monitorv3.py
+++ backend-southern-exposure/monitorv3.py
@@ -55,46 +55,55 @@ def worker(name_var):
           print("I'm alive " + name_var)
           continue
     
+def get_valid_processes(sqliteConnection):
+    try:
+        cursor = sqliteConnection.cursor()
+        sqlite_select_Query = "SELECT * FROM jobs;"
+        cursor.execute(sqlite_select_Query)
+        sqliteConnection.row_factory = sqlite3.Row
+        record = cursor.fetchall()
+        for row in record:
+            if len(row[2]) >= 4:
+                time_list = row[2].split(",")
+                for item in time_list:
+                    schedule_item = row[1]+item+str(row[3])
+                    jobs_on_table.append(schedule_item)
+        cursor.close()
+        return jobs_on_table
+    except sqlite3.Error as error:
+        raise error
 
-def connect_db():
-    while True:
-        try:
-            sqliteConnection = sqlite3.connect('./southern_exposure_database.db')
-            cursor = sqliteConnection.cursor()
-            sqlite_select_Query = "SELECT * FROM jobs;"
-            cursor.execute(sqlite_select_Query)
-            sqliteConnection.row_factory = sqlite3.Row
-            record = cursor.fetchall()
-            jobs_on_table = []
-            for row in record:
-                if len(row[2]) >= 4:
-                    time_list = row[2].split(",")
-                    for item in time_list:
-                        schedule_item = row[1]+item+str(row[3])
-                        jobs_on_table.append(schedule_item)
-            if sorted(jobs_on_table) != sorted(accepted_processes):
-                print("Jobs on Table Do Not Match Schedule")
-                for each in list(set(accepted_processes) - set(jobs_on_table)):
-                    accepted_processes.remove(each)
-                for each  in list(set(jobs_on_table) - set(accepted_processes)):
-                    accepted_processes.append(each)
-                    p = multiprocessing.Process(target=worker, args=(each,))
-                    jobs.append(p)
-                    p.start()
-                with open('./definitions.csv', 'w') as outfile:
-                    writer = csv.writer(outfile)
-                    for x in accepted_processes : writer.writerow ([x])  
-            else:
-                cursor.close()
-                continue
-
-        except sqlite3.Error as error:
-            print("Error while connecting to sqlite", error)
-        finally:
-            if sqliteConnection:
-                sqliteConnection.close()
-
-if __name__ == '__main__':
+def connect_db(sqliteConnection):
     jobs = []  # this is the array that is keeping these multiprocesses alive
     accepted_processes = []  # this is the array of what should alive at any one time
-    connect_db()
+    while True:
+        try:
+            jobs_on_table = get_valid_processes(sqliteConnection)
+        except Exception as e:
+            raise e
+            return
+        if sorted(jobs_on_table) != sorted(accepted_processes):
+            print("Jobs on Table Do Not Match Schedule")
+            for each in list(set(accepted_processes) - set(jobs_on_table)):
+                accepted_processes.remove(each)
+            for each  in list(set(jobs_on_table) - set(accepted_processes)):
+                print("Appending process: "+each)
+                accepted_processes.append(each)
+                p = multiprocessing.Process(target=worker, args=(each,))
+                jobs.append(p)
+                p.start()
+            with open('./definitions.csv', 'w') as outfile:
+                writer = csv.writer(outfile)
+                for x in accepted_processes : writer.writerow ([x])  
+        else:
+            time.sleep(5)
+
+
+if __name__ == '__main__':
+    try:
+        sqliteConnection = sqlite3.connect('./southern_exposure_database.db')
+    except Exception as e:
+        print(e)
+        exit
+    connect_db(sqliteConnection)
+    sqliteConnection.close()
